9d39f5d2e7a6dde40b8ffb1469dda48264ea5847
Tina Schönborn <tina.schoenborn@student.htw-berlin.de>
2016-11-17 21:48:31 +0100

    add user changeable column numbers

--- catlendar.rb
+++ catlendar.rb
@@ -4,17 +4,33 @@ require 'date'
 
 puts "How many days from now on you want to print?"
 days = STDIN.gets.strip
+puts "How many columns do you want to have?"
+columns = STDIN.gets.strip
 puts "Thank you. The PDF will be generated in a second."
 
 start_date = Date.today
 end_date = start_date + days.to_i
+columns = columns.to_i
 
 Prawn::Document.generate("hello.pdf") do
 
+  FULL_COLUMN_WIDTH = 264
+  FIRST_COLUMN_LINE =  80
+  SECOND_COLUMN_LINE = 360
+  COLUMN_WIDTH = FULL_COLUMN_WIDTH - FIRST_COLUMN_LINE
+
   repeat :all do
     stroke do
-      vertical_line 0, bounds.height, :at => 80
-      vertical_line 0, bounds.height, :at => 360
+      vertical_line 0, bounds.height, :at => FIRST_COLUMN_LINE
+      vertical_line 0, bounds.height, :at => SECOND_COLUMN_LINE
+      vertical_line 0, bounds.height, :at => FULL_COLUMN_WIDTH
+      vertical_line 0, bounds.height, :at => bounds.width
+    end
+    for i in 1...columns do 
+      stroke do
+        vertical_line 0, bounds.height, :at => FIRST_COLUMN_LINE + (COLUMN_WIDTH/columns) * i
+        vertical_line 0, bounds.height, :at => SECOND_COLUMN_LINE + (COLUMN_WIDTH/columns) * i
+      end
     end
   end
 
